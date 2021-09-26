a4fd222877125eb67f4fd4ff16e510725f3f522f
sajad <sghoboor20@gmail.com>
2021-09-26 13:25:12 +0330

    edit

--- functions.php
+++ functions.php
@@ -7,8 +7,18 @@
  
 function aquila_load_scripts(){
 
-wp_enqueue_style('stylesheet',get_stylesheet_uri(),array(),filemtime(get_template_directory().'/style.css'),'all');
-// wp_enqueue_style('custom',get_template_directory_uri().'/custom.css',array('stylesheet'),'1','all');
+
+
+wp_register_style('style-css',get_stylesheet_uri(),array(),filemtime(get_template_directory().'/style.css'),'all');
+wp_enqueue_style('style-css');
+
+
+wp_register_script('main-js',get_template_directory_uri().'/assets/main.js',array(),false,true);
+wp_enqueue_script('main-js');
+
+
+// wp_enqueue_style('stylesheet',get_stylesheet_uri(),array(),filemtime(get_template_directory().'/style.css'),'all');
+// // wp_enqueue_style('custom',get_template_directory_uri().'/custom.css',array('stylesheet'),'1','all');
 
 }
 add_action('wp_enqueue_scripts','aquila_load_scripts');
